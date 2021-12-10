a13db025f2722f7b4f9d75eded763dbb14293478
Geovane Araújo <geovane-araujo@outlook.com>
2021-12-10 12:13:09 -0300

    repository

---
+++ Banco/TerminalBancario/BancoCore/ContaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCore
+{
+    class ContaRepository
+    {
+        private readonly bancodbEntities _entities;
+        public ContaRepository()
+        {
+            _entities = new bancodbEntities();
+        }
+    }
+}
